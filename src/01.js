2b2b86985725f9e9f06199a902e9babc7f41c08d
David Moriez <61180214+capmaravilla@users.noreply.github.com>
2024-11-01 23:28:40 +0100

    Primer bloque de 5 ejercicios

--- src/01.js
+++ src/01.js
@@ -58,5 +58,98 @@ function uniqueInOrder2(iterable) {
 
 // console.log(uniqueInOrder([1, 2, 2, 3, 3]));
 // console.log(uniqueInOrder('ABBCcAD'));
-// console.log(uniqueInOrder2([1, 2, 2, 3, 3]));
-// console.log(uniqueInOrder2('ABBCcAD'));
+
+//* ---> 6
+//? Your task is to sort a given string. Each word in the string will contain a single number. This number is the position the word should have in the result.
+//? Note: Numbers can be from 1 to 9. So 1 will be the first word (not 0).
+//? If the input string is empty, return an empty string. The words in the input String will only contain valid consecutive numbers.
+// "is2 Thi1s T4est 3a"  -->  "Thi1s is2 3a T4est"
+// "4of Fo1r pe6ople g3ood th5e the2"  -->  "Fo1r the2 g3ood 4of th5e pe6ople"
+// ""  -->  ""
+
+function order(words) {
+	arr = words.split(' ');
+	result = [];
+	for (const el of arr) {
+		result[el.match(/\d+/) - 1] = el;
+		// \d representa cualquier dígito (del 0 al 9)
+		// + significa "uno o más", para capturar secuencias de dígitos completas
+	}
+	return result.join(' ');
+}
+
+function order2(words) {
+	return words
+		.split(' ')
+		.sort((a, b) => a.match(/\d/) - b.match(/\d/))
+		.join(' ');
+	//TODO Buen ejemplo del uso de SORT()
+}
+
+// console.log(order('is2 Thi1s T4est 3a'));
+
+//* ---> 6 Tribonacci Sequence
+//? Well met with Fibonacci bigger brother, AKA Tribonacci.
+//? As the name may already reveal, it works basically like a Fibonacci, but summing the last 3 (instead of 2) numbers of the sequence to generate the next. And, worse part of it, regrettably I won't get to hear non-native Italian speakers trying to pronounce it :(
+//? So, if we are to start our Tribonacci sequence with [1, 1, 1] as a starting input (AKA signature), we have this sequence:
+// [1, 1 ,1, 3, 5, 9, 17, 31, ...]
+//? But what if we started with [0, 0, 1] as a signature? As starting with [0, 1] instead of [1, 1] basically shifts the common Fibonacci sequence by once place, you may be tempted to think that we would get the same sequence shifted by 2 places, but that is not the case and we would get:
+// [0, 0, 1, 1, 2, 4, 7, 13, 24, ...]
+//? Well, you may have guessed it by now, but to be clear: you need to create a fibonacci function that given a signature array/list, returns the first n elements - signature included of the so seeded sequence.
+//? Signature will always contain 3 numbers; n will always be a non-negative number; if n == 0, then return an empty array (except in C return NULL) and be ready for anything else which is not clearly specified ;)
+
+function tribonacci(signature, n) {
+	if (n === 0) {
+		return [];
+	}
+
+	if (typeof n !== 'number') {
+		return null;
+	}
+
+	if (n <= 3) {
+		return signature.slice(0, n);
+	}
+
+	arrTri = [...signature];
+	for (let el = 3; el < n; el++) {
+		arrTri.push(arrTri[el - 1] + arrTri[el - 2] + arrTri[el - 3]);
+	}
+	return arrTri;
+}
+
+function tribonacci2(signature, n) {
+	for (let i = 0; i < n - 3; i++) {
+		// iterate n times
+		signature.push(signature[i] + signature[i + 1] + signature[i + 2]); // add last 3 array items and push to trib
+	}
+	return signature.slice(0, n); //return trib - length of n
+}
+
+// console.log(tribonacci([1, 1, 1], 10));
+// console.log(tribonacci2([1, 1, 1], 4));
+
+//* --->6
+//? A pangram is a sentence that contains every single letter of the alphabet at least once. For example, the sentence "The quick brown fox jumps over the lazy dog" is a pangram, because it uses the letters A-Z at least once (case is irrelevant).
+//? Given a string, detect whether or not it is a pangram. Return True if it is, False if not. Ignore numbers and punctuation.
+
+function isPangram(string) {
+	let check = true;
+	const alphabet = 'abcdefghijklmnopqrstuvwxyz';
+	for (const el of alphabet) {
+		if (!string.toLowerCase().includes(el)) {
+			check = false;
+		}
+	}
+	return check;
+}
+
+function isPangram2(string) {
+	str = string.toLowerCase();
+	return 'abcdefghijklmnopqrstuvwxyz'
+		.split('')
+		.every((x) => str.indexOf(x) !== -1);
+}
+
+// console.log(isPangram('The quick brown fox jumps over the lazy dog.'));
+// console.log(isPangram('This is not a pangram.'));
